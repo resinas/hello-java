cf116b6941336d679a339d5b21efe65aa468bafb
Manuel <manuelgomezsuarez@gmail.com>
2017-12-11 13:09:32 +0100

    probando error

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,7 +3,7 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.builder.SpringApplicationBuilder
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
