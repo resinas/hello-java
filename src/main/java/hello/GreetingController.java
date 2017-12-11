fd9fc22afc17b3d37ff647846dbc5cc012bb20dc
Domingo <dmunnoz96@gmail.com>
2017-12-11 09:30:48 +0100

    fixing language greeting

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
+    @Autowired
+    private GreetingTranslator greetingTranslator;
+
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name,@RequestParam(value="lang", required=false, defaultValue="en") String lang, Model model) {
 	model.addAttribute("hello", greetingTranslator.sayHelloIn(lang));
