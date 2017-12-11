cd01fd226e54552bf0dca669494617cac9616790
danrodagu <danrodagu@alum.us.es>
2017-12-11 13:42:07 +0100

    cambios en codigo

---
+++ src/main/java/hello/GreetingTranslator.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class GreetingTranslator {
+	
+	public String sayHelloIn(String lang) {
+		String hello;
+		if ("en".equals(lang)) {
+			hello = "hello";
+		} else if ("es".equals(lang)) {
+			hello = "hola";
+		} else {
+			hello = "no hablo tu idioma";
+		}
+	}
+
