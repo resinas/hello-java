b11748dd2bdd35c91a9dce727944c6d0566bbd0e
Domingo <dmunnoz96@gmail.com>
2017-12-11 09:34:04 +0100

    removing @autowired symbol

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @Autowired
     private GreetingTranslator greetingTranslator;
 
     @RequestMapping("/greeting")
