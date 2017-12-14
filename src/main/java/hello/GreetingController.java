82cb087fcc4f551016d2b306dcb6c7bcb19bfc40
ilFreddo48 <mohalamha@alum.us.es>
2017-12-14 18:46:37 +0100

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import hello.GreetingTranslator ;
 
+@Controller
 public class GreetingController{
 
     @RequestMapping("/greeting")
