a8ceaca76b52633d853bd83a44e610f47c728e64
Pierrick <pierrickjr@gmail.com>
2017-12-14 18:11:13 +0100

    Flujo de trabajo, modificacion

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -9,10 +9,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, 
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name,
     					   Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+                   model.addAttribute("name", name);
+                   return "greeting";
     }
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name,
+               @RequestParam(value="lang", required=false, defaultValue="en") String lang,
+               Model model) {
+                 model.addAttribute("hello", greetingTranslator.sayHelloIn(lang));
+                 model.addAttribute("name", name);
+                 return "greeting";
+  }
 
 }
