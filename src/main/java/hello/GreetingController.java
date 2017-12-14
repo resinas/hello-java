0a19ceb6bb3dc47f720a88cc2d9d10e74f27d96a
ilFreddo48 <mohalamha@alum.us.es>
2017-12-14 18:30:18 +0100

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,13 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import hello.GreetingTranslator
 
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, 
     					   @RequestParam(value="lang", required=false, defaultValue="en") String lang, 
     					   Model model) {
-    	model.addAttribute("hello", greetingTranslator.sayHelloIn(lang));
+    	model.addAttribute("hello", GreetingTranslator.sayHelloIn(lang));
         model.addAttribute("name", name);
         return "greeting";
     }
