58fd0716e5a7a1a833067feecc0ce387a8eee9c1
Víctor Rosa <vicrosa25@gmail.com>
2018-01-08 11:38:01 +0100

    Se añade add-language

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -5,14 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
-public class GreetingController {
-
-    @RequestMapping("/greeting")
+@RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, 
+    					   @RequestParam(value="lang", required=false, defaultValue="en") String lang, 
     					   Model model) {
+    	model.addAttribute("hello", greetingTranslator.sayHelloIn(lang));
         model.addAttribute("name", name);
         return "greeting";
     }
-
-}
---
+++ src/main/java/hello/GreetingTranslator.java
@@ -0,0 +1,14 @@
+public class GreetingTranslator {
+	
+	public String sayHelloIn(String lang) {
+		String hello;
+		if ("en".equals(lang)) {
+			hello = "hello";
+		} else if ("es".equals(lang)) {
+			hello = "hola";
+		} else {
+			hello = "no hablo tu idioma";
+		}
+	}
+
+}
