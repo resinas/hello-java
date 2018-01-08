1e14322001b65dd60aa8f7011c477dba56415ecb
Víctor Rosa <vicrosa25@gmail.com>
2018-01-08 12:43:11 +0100

    Corregido GreetingController

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -5,7 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@RequestMapping("/greeting")
+@Controller
+public class GreetingController {
+    @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, 
     					   @RequestParam(value="lang", required=false, defaultValue="en") String lang, 
     					   Model model) {
@@ -13,3 +15,4 @@ import org.springframework.web.bind.annotation.RequestParam;
         model.addAttribute("name", name);
         return "greeting";
     }
+}
