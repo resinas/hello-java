1485278328b28843f07fc768ae6408a60e6226c1
fedplagar <alvaro.pg96@gmail.com>
2018-01-14 06:59:38 +0100

    corregido mas fallos

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,15 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import hello.GreetingTranslator ;
 
 @Controller
-public class GreetingController {
+public class GreetingController{
 
 	@RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, 
     					   @RequestParam(value="lang", required=false, defaultValue="en") String lang, 
     					   Model model) {
-    	model.addAttribute("hello", greetingTranslator.sayHelloIn(lang));
+    	model.addAttribute("hello", GreetingTranslator.sayHelloIn(lang));
         model.addAttribute("name", name);
         return "greeting";
     }
