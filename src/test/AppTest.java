cee2cdf006b6b826e0c026d72dc38843c6398c65
fedplagar <alvaro.pg96@gmail.com>
2018-01-15 01:45:57 +0100

    anade los test

---
+++ src/test/AppTest.java
@@ -0,0 +1,23 @@
+package es.egc.app;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+
+
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue(true);
+    }
+}
